c1d6eec88aa05460f8bf994d5e098d1b1c9eb06a
Gustavo Inacio <gus.inacio@hotmail.com>
2024-02-25 11:37:10 -0300

    feat: initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import GlobalStylesComponent from '@/styles/globals'
+import ThemeConfig from '@/styles/theme'
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider {...pageProps}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <GlobalStylesComponent />
+      <ThemeConfig>
+        <Component {...pageProps} />
+      </ThemeConfig>
+    </AppCacheProvider>
+  )
 }
